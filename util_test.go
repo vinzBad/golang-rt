07b89694849f5396540ac368d5f0b87a413262e0
Vinzenz Sinapius <vinzenz.sinapius@gmail.com>
2019-09-08 22:13:09 +0200

    add test for parseRTResponseKVs

--- util_test.go
+++ util_test.go
@@ -21,3 +21,27 @@ func TestParseRtResponseHeader(t *testing.T) {
 		t.Error("Parsed header didn' match expected header")
 	}
 }
+
+func TestParseRTResponseKVs(t *testing.T) {
+	_, err := parseRTResponseKVs([]byte(`<!DOCTYPE html>`))
+	if err != ErrParseRTMessageError {
+		t.Errorf("parseRTResponseKVs didn't detect faulty message")
+	}
+
+	result, _ := parseRTResponseKVs([]byte(`
+	foo: bar
+	yes: yaa
+	`))
+
+	expectedResult := map[string]string{
+		"foo": "bar",
+		"yes": "yaa",
+	}
+
+	for k, v := range expectedResult {
+		if result[k] != v {
+			t.Errorf("Expected %q: %q got %q", k, v, result[k])
+		}
+	}
+
+}
